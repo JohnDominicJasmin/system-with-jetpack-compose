1023129330f3c036608a277b52a24cf66eb0738a
John_Dominic <johndominicjasmin@gmail.com>
2022-04-25 19:34:44 +0800

    Write test for validation.

--- src/test/kotlin/Test.kt
+++ src/test/kotlin/Test.kt
@@ -1,24 +1,65 @@
 
+import kotlinx.coroutines.runBlocking
+import resident_feature.domain.model.Resident
+import resident_feature.domain.util.validateInput
 import java.text.SimpleDateFormat
+import java.util.*
 import kotlin.test.Test
+
 
 class Test {
     @Test
     fun test() {
-        println(assert( isValidDate("45/23/234")))
-        println(assert(isValidDate("12/12/2111")))
+        runBlocking {
+            validateInput(
+                resident = Resident(
+                    fullName = "Jose",
+                    sex = "male",
+                    suffix = "Jr",
+                    address = "Manila",
+                    religion = "Muslim",
+                    dateOfBirth = "24/09/2001",
+                    contactNumber = "09194341361",
+                    purok = "Purok Wan",
+                    civilStatus = "Single",
+                    occupation = "Ninja",
+                    voter = "Yes",
+                    citizenship = "Peenoise",
+                    seniorCitizen = "Yes",
+                    educationalAttainment = "College",
+                    imageName = "George",
+                    localImagePath = "sample_path_to_heaven",
+                    age = getAge(dobString = "24/09/2001").toString()
+                )
+            ) {
+                print("Success\n\n")
+                print("Age is ${getAge(dobString = "24/09/2001")}")
+            }
+        }
+
     }
 
-    private fun isValidDate(input: String): Boolean {
-        val formatString = "MM/dd/yyyy"
+    private fun getAge(dobString: String): Int {
+        var date: Date? = null
+        val sdf = SimpleDateFormat("dd/MM/yyyy")
 
-        return kotlin.runCatching {
-            val format = SimpleDateFormat(formatString)
-            format.isLenient = false
-            format.parse(input)
-        }.isSuccess
+        kotlin.runCatching {
+            date = sdf.parse(dobString)
+        }.onFailure { print("Failed") }
 
 
+        if (date == null) return 0
+        val dob = Calendar.getInstance()
+        val today = Calendar.getInstance()
+        dob.time = date
+        val year = dob[Calendar.YEAR]
+        val month = dob[Calendar.MONTH]
+        val day = dob[Calendar.DAY_OF_MONTH]
+        dob[year, month + 1] = day
+        var age = today[Calendar.YEAR] - dob[Calendar.YEAR]
+        if (today[Calendar.DAY_OF_YEAR] < dob[Calendar.DAY_OF_YEAR]) {
+            age--
+        }
+        return age
     }
-
 }
