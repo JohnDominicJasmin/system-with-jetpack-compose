7f73017f2e58153067f2140999f8a063cf2546b3
John_Dominic <johndominicjasmin@gmail.com>
2022-05-01 21:17:40 +0800

    Move some icons and add icons for dialog.

--- src/main/kotlin/resident_feature/util/DrawableResource.kt
+++ src/main/kotlin/resident_feature/util/DrawableResource.kt
@@ -1,12 +1,14 @@
 package resident_feature.util
 
 sealed class DrawableResource(val resource: String){
- object TrailingIconUp: DrawableResource(resource = "ic_trailing_icon_up.svg")
- object TrailingIconDown: DrawableResource(resource = "ic_trailing_icon_down.svg")
- object ProfilePlaceHolder: DrawableResource(resource = "profile_placeholder.jpg")
- object SortIcon: DrawableResource(resource = "ic_sort_icon.svg")
- object ToggleSortIcon: DrawableResource(resource = "ic_toggle_sort_icon.svg")
- object EditIcon: DrawableResource(resource = "ic_pen.svg")
- object DeleteIcon: DrawableResource(resource = "ic_trash.svg")
-
+ object TrailingIconUp: DrawableResource(resource = "system_resources/ic_trailing_icon_up.svg")
+ object TrailingIconDown: DrawableResource(resource = "system_resources/ic_trailing_icon_down.svg")
+ object ProfilePlaceHolder: DrawableResource(resource = "system_resources/profile_placeholder.jpg")
+ object SortIcon: DrawableResource(resource = "system_resources/ic_sort_icon.svg")
+ object ToggleSortIcon: DrawableResource(resource = "system_resources/ic_toggle_sort_icon.svg")
+ object EditIcon: DrawableResource(resource = "system_resources/ic_pen.svg")
+ object DeleteIcon: DrawableResource(resource = "system_resources/ic_trash.svg")
+ object SuccessIcon: DrawableResource(resource = "system_resources/ic_success.svg")
+ object FailedIcon: DrawableResource(resource = "system_resources/ic_error.svg")
+ object WarningIcon: DrawableResource(resource = "system_resources/ic_warning.svg")
 }
