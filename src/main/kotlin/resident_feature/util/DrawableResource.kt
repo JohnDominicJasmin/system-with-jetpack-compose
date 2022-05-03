91ef52f32cadc5bda6ee4af2779c3efe916483c6
John_Dominic <johndominicjasmin@gmail.com>
2022-05-03 20:47:41 +0800

    Change icon whether ascending or descending.

--- src/main/kotlin/resident_feature/util/DrawableResource.kt
+++ src/main/kotlin/resident_feature/util/DrawableResource.kt
@@ -4,12 +4,17 @@ sealed class DrawableResource(val resource: String){
  object TrailingIconUp: DrawableResource(resource = "system_resources/ic_trailing_icon_up.svg")
  object TrailingIconDown: DrawableResource(resource = "system_resources/ic_trailing_icon_down.svg")
  object ProfilePlaceHolder: DrawableResource(resource = "system_resources/profile_placeholder.jpg")
- object SortIcon: DrawableResource(resource = "system_resources/ic_sort_icon.svg")
- object ToggleSortIcon: DrawableResource(resource = "system_resources/ic_toggle_sort_icon.svg")
+
+ object SortIconDescending: DrawableResource(resource = "system_resources/ic_sort_down.svg")
+ object SortIconAscending: DrawableResource(resource = "system_resources/ic_sort_up.svg")
+
+ object ToggledIcon: DrawableResource(resource = "system_resources/ic_toggle_up.svg")
+ object NotToggledIcon: DrawableResource(resource = "system_resources/ic_toggle_down.svg")
+
  object EditIcon: DrawableResource(resource = "system_resources/ic_pen.svg")
  object DeleteIcon: DrawableResource(resource = "system_resources/ic_trash.svg")
  object SuccessIcon: DrawableResource(resource = "system_resources/ic_success.svg")
- object FailedIcon: DrawableResource(resource = "system_resources/ic_error.svg")
+ object ErrorIcon: DrawableResource(resource = "system_resources/ic_error.svg")
  object WarningIcon: DrawableResource(resource = "system_resources/ic_warning.svg")
  object QuestionIcon: DrawableResource(resource = "system_resources/ic_question.svg")
  object NoImageFound: DrawableResource(resource = "system_resources/ic_no_image_found.jpg")
